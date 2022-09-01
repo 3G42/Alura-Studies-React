0db29355cc9ab42e8ef02b6326c7f1b385c2f050
Gabriel Gomes <gabriel.garcia1@ufu.br>
2022-09-01 20:14:58 -0300

    Cria componentes estáticos

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./style.module.scss";
+
+class Botao extends React.Component<{ children: string }> {
+  render() {
+    return <button className={style.botao}>{this.props.children}</button>;
+  }
+}
+
+export default Botao;
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Item from "./Item";
+import style from "./style.module.scss";
+
+function Lista() {
+  const [tarefas, setTarefas] = useState([
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Javascript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "Python",
+      tempo: "01:30:00",
+    },
+  ]);
+  return (
+    <aside className={style.listaTarefas}>
+      <h2
+        onClick={() => {
+          console.log("h2 clicado", tarefas);
+          setTarefas([
+            ...tarefas,
+            { tarefa: "Arrasando no estado", tempo: "01:00:00" },
+          ]);
+        }}
+      >
+        Estudos do dia
+      </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
